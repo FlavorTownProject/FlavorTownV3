307f406947dede375025b28b0a341402a9ad6f20
Austyn Trull <agtrull@crimson.ua.edu>
2016-11-16 15:45:56 -0600

    Firebase Classes
    These files are for the Firebase queries.

---
+++ app/src/main/java/cs/ua/edu/flavortown/RestaurantInfo.java
@@ -0,0 +1,22 @@
+public class RestaurantInfo {
+    int latitude;
+    int longitude;
+    String restName;
+    Menu menu;
+
+    public RestaurantInfo() {
+        // empty default constructor, necessary for Firebase to be able to deserialize blog posts
+    }
+    public int getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public long getRestName() {
+        return restName;
+    }
+    public Menu getMenu(){
+        return menu;
+    }
+}
