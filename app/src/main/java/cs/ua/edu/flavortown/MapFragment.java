6063e47cb969da10e16c072f236c48674683750b
Ian Campbell <imcampbell@crimson.ua.edu>
2016-11-30 20:11:25 -0600

    Fix the menu display glitches, working on listeners

--- app/src/main/java/cs/ua/edu/flavortown/MapFragment.java
+++ app/src/main/java/cs/ua/edu/flavortown/MapFragment.java
@@ -65,8 +65,9 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 
     //original key: AIzaSyDZzxLsGBZ2aefPmsyGzpdB63OVpvc8PNY
     //Back up Key (Flavortown): 	AIzaSyA2pbTswWHv5NslX4e35AWEu-PiCbMe3wY
-    private static String gpURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?&key=AIzaSyA2pbTswWHv5NslX4e35AWEu-PiCbMe3wY&query=hamburgers";//AIzaSyDU5KCvghYUqvJdkMY7OBo2mr8jsAEvHqY";
-    static String detailURL = "https://maps.googleapis.com/maps/api/place/details/json?&key=AIzaSyA2pbTswWHv5NslX4e35AWEu-PiCbMe3wY";
+    //Back up 2: AIzaSyDK6GB-RTPuNRZIKpsLd0mUE667t3KXqM4
+    private static String gpURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?&key=AIzaSyDK6GB-RTPuNRZIKpsLd0mUE667t3KXqM4&query=hamburgers";//AIzaSyDU5KCvghYUqvJdkMY7OBo2mr8jsAEvHqY";
+    static String detailURL = "https://maps.googleapis.com/maps/api/place/details/json?&key=AIzaSyDK6GB-RTPuNRZIKpsLd0mUE667t3KXqM4";
 
 
     private JSONObject jsonIDS;
--- app/src/main/java/cs/ua/edu/flavortown/MenuActivity.java
+++ app/src/main/java/cs/ua/edu/flavortown/MenuActivity.java
@@ -102,12 +102,21 @@ public class MenuActivity extends AppCompatActivity {
                                 Log.v(LOGTAG, "flag = "+ temp.getFlag());
                                 Log.v(LOGTAG, "adding food to menu");
                                 currMenu.addToFoodList(temp);
-
+                                Log.v(LOGTAG, "First two menu items after adding");
+                                Log.v(LOGTAG, "0 = " + currMenu.foodList[0].getFoodItem());
+                                Log.v(LOGTAG, "1 = " + currMenu.foodList[1].getFoodItem());
                             }
                             if(currMenu.length > 0){
                                 Log.v(LOGTAG, "Populated menu");
                                 //currMenu.sortItemsByRating();
+                                Log.v(LOGTAG, "First two menu items before setting currRest");
+                                Log.v(LOGTAG, "0 = " + currMenu.foodList[0].getFoodItem());
+                                Log.v(LOGTAG, "1 = " + currMenu.foodList[1].getFoodItem());
+                                currMenu.sortItemsByRating();
                                 currentRestuarant.setMenu(currMenu);
+                                Log.v(LOGTAG, "First two menu items after setting currRest");
+                                Log.v(LOGTAG, "0 = " + currMenu.foodList[0].getFoodItem());
+                                Log.v(LOGTAG, "1 = " + currMenu.foodList[1].getFoodItem());
                                 populateMenu(true);
                             }
                             else
@@ -149,7 +158,7 @@ public class MenuActivity extends AppCompatActivity {
         Food[] pullMenu = currentRestuarant.getMenu().getFoodList();
         String listItems[] = new String[currentRestuarant.getMenu().length];
         for(int x = 0; x < pullMenu.length; x++) {
-
+            Log.v(LOGTAG, "pullMenu[x] = "+ pullMenu[x].getFoodItem());
             listItems[x] = pullMenu[x].getFoodItem();
             Log.v(LOGTAG, x + " = " + listItems[x]);
         }
